07360481dffe48e69572a528a7f867eeabe2308a
Michael MacFadden <michael@convergencelabs.com>
2019-12-14 17:43:59 -0600

    Removed the unneeded command in the prepare.sh.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -12,6 +12,3 @@ cp -a src/* build
 CONVERGENCE_URL=https://nexus.dev.convergencelabs.tech/repository/maven-all/com/convergencelabs/convergence-server/$CONVERGENCE_VERSION/convergence-server-$CONVERGENCE_VERSION.tgz
 echo "Downloading Convergence Server $CONVERGENCE_VERSION from: $CONVERGENCE_URL"
 curl $CONVERGENCE_URL --output build/convergence-server.tgz
-
-# Run the docker build
-docker build -t convergencelabs/convergence-server build
